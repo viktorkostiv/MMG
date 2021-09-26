dfc9e525fd1305284d88fa2ee8ac1dfaaf59398a
viktorkostiv <71613073+viktorkostiv@users.noreply.github.com>
2021-09-26 03:48:22 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+document.getElementById('menuOn').addEventListener('click', () => {
+  document.querySelector('.nav').classList.add('active');
+});
+document.getElementById('menuOff').addEventListener('click', () => {
+  document.querySelector('.nav').classList.remove('active');
+});
+$(document).ready(function () {
+    $('.head-slider').slick({
+        dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+    });
+    $('.services-slider').slick({
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                arrows: false,
+                dots: true,
+              }
+            },
+          ]
+    });
+    $('.partners-slider').slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                  slidesToShow: 1,
+                }
+              },
+        ]
+    });
+    $('.coments-slider').slick({
+        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                arrows: false,
+                dots: true,
+              }
+            },
+          ]
+    });
+  });
